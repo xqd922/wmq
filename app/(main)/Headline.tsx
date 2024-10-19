b4c19e68312dea9b0e72d64ae2aa9cda41efdb07
xqd922 <160695918+xqd922@users.noreply.github.com>
2024-10-19 18:45:43 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -4,13 +4,13 @@ import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
 
 import { SparkleIcon, UserSecurityIcon } from '~/assets'
-import { PeekabooLink } from '~/components/links/PeekabooLink'
+//import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { SocialLink } from '~/components/links/SocialLink'
 
 function Developer() {
   return (
     <span className="group">
-      <span className="font-mono">&lt;</span>开发者
+      <span className="font-mono">&lt;</span>大学生
       <span className="font-mono">/&gt;</span>
       <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
     </span>
@@ -26,7 +26,7 @@ function Designer() {
         <span className="absolute -bottom-[3.5px] -left-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
         <span className="absolute -right-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
       </span>
-      设计师
+      待定
     </span>
   )
 }
@@ -35,7 +35,7 @@ function OCD() {
   return (
     <span className="group inline-flex items-center">
       <SparkleIcon className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
-      <span>细节控</span>
+      <span>待定</span>
     </span>
   )
 }
@@ -44,7 +44,7 @@ function Founder() {
   return (
     <span className="group inline-flex items-center">
       <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
-      <span>创始人</span>
+      <span>待定</span>
     </span>
   )
 }
@@ -80,10 +80,7 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 Cali，
-          <PeekabooLink href="https://zolplay.com">佐玩</PeekabooLink>
-          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
-          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
+        hi! 大家好，我是xqd，目前是一名在校大学生，一个喜欢折腾的人，热衷于探索和尝试各种新事物。
         </Balancer>
       </motion.p>
       <motion.div
@@ -98,6 +95,7 @@ export function Headline() {
           delay: 0.25,
         }}
       >
+        {/*   
         <SocialLink
           href="https://cali.so/twitter"
           aria-label="我的推特"
@@ -113,19 +111,22 @@ export function Headline() {
           aria-label="我的 Bilibili"
           platform="bilibili"
         />
+         */}
         <SocialLink
-          href="https://cali.so/github"
+          href="https://github.com/xqd922"
           aria-label="我的 GitHub"
           platform="github"
         />
         <SocialLink
-          href="https://cali.so/tg"
+          href="https://t.me/xqd770_bot"
           aria-label="我的 Telegram"
           platform="telegram"
         />
+        {/*
         <SocialLink href="/feed.xml" platform="rss" aria-label="RSS 订阅" />
+        */}
         <SocialLink
-          href="mailto:hi@cali.so"
+          href="xqd922@gmail.com"
           aria-label="我的邮箱"
           platform="mail"
         />
