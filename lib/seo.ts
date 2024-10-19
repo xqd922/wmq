99d728e0db8d2ad43762953c6b6d3658a7c3832f
xqd922 <160695918+xqd922@users.noreply.github.com>
2024-10-19 18:46:13 +0800

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,10 +1,10 @@
 export const seo = {
-  title: 'Cali Castle | 开发者、设计师、细节控、创始人',
+  title: 'xqd blog',
   description:
-    '我叫 Cali，一名开发者，设计师，细节控，同时也是佐玩创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。',
+    'hi! 大家好，我是xqd，目前是一名在校大学生，一个喜欢折腾的人，热衷于探索和尝试各种新事物。',
   url: new URL(
     process.env.NODE_ENV === 'production'
-      ? 'https://cali.so'
+      ? 'https://xqd.us.kg'
       : 'http://localhost:3000'
   ),
 } as const
